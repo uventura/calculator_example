9d49787251e5e9a711deb57a40b39ee2d0d80dfd
uventura <ualivent@gmail.com>
2023-10-07 14:10:29 -0300

    Adding Library Calculator

---
+++ lib/calculator.c
@@ -0,0 +1,26 @@
+#include "calculator.h"
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+
+int sub(int a, int b)
+{
+    return a-b;
+}
+
+int div(int a, int b)
+{
+    return a/b;
+}
+
+int mult(int a, int b)
+{
+    return a*b;
+}
+
+int abs(int a)
+{
+    return a < 0? a : -a;
+}
